3ff68252fdeb44dd2be99e711ced2ddff557927b
Kevinclane <kevinclane21@gmail.com>
2025-01-04 16:35:53 -0700

    reinit

--- src/main/java/com/allergenie/api/AllergenieApiApplication.java
+++ src/main/java/com/allergenie/api/AllergenieApiApplication.java
@@ -11,6 +11,8 @@ public class AllergenieApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AllergenieApiApplication.class, args);
 
+
+
 		System.out.println("Application Starting");
 		System.out.println("Serving on Port: 8080");
 	}
