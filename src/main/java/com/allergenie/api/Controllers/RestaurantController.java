c2f6f0805930b9bb0ada9f02ad0ad32f3d2f677c
Kevinclane <kevinclane21@gmail.com>
2025-01-06 16:13:55 -0700

    adds menu controller path and logic to get menus for each restaurant

--- src/main/java/com/allergenie/api/Controllers/RestaurantController.java
+++ src/main/java/com/allergenie/api/Controllers/RestaurantController.java
@@ -1,34 +1,46 @@
 package com.allergenie.api.Controllers;
 
 import com.allergenie.api.Models.Entities.Restaurant;
-import com.allergenie.api.Models.Responses.TempResponse;
 import com.allergenie.api.Services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Collections.emptyList;
 
 @RestController
-@RequestMapping(value = "/restaurant")
+@RequestMapping(value = "/api/restaurant")
 public class RestaurantController {
 
     @Autowired
     private RestaurantService restaurantService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<TempResponse>> getRestaurants() {
+    public ResponseEntity<List<Restaurant>> getRestaurants() {
         try {
-            List<TempResponse> restaurants = restaurantService.getRestaurants();
+            List<Restaurant> restaurants = restaurantService.getRestaurants();
             return new ResponseEntity<>(restaurants, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(emptyList(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurant> getRestaurantById(Integer id) {
+        try {
+            Optional<Restaurant> restaurant = restaurantService.getRestaurantById(id);
+            if(restaurant.isEmpty()) {
+                return new ResponseEntity<>(new Restaurant(), HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(restaurant.get(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new Restaurant(), HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/main/java/com/allergenie/api/Models/Entities/Menu.java
+++ src/main/java/com/allergenie/api/Models/Entities/Menu.java
@@ -4,13 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Entity
+@Data
 public class Menu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private Boolean isActive;
+
+    public Menu(ResultSet rs) throws SQLException {
+        this.id = rs.getInt("id");
+        this.name = rs.getString("name");
+        this.isActive = rs.getBoolean("isactive");
+    }
 
 }
