b7c4f56e0b7a732bf1f4aff42d164146a20a18fb
Kevinclane <kevinclane21@gmail.com>
2025-03-07 14:17:26 -0700

    adds logic to save and validate restaurants

--- src/main/java/com/allergenie/api/Controllers/RestaurantController.java
+++ src/main/java/com/allergenie/api/Controllers/RestaurantController.java
@@ -5,9 +5,7 @@ import com.allergenie.api.Services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,6 +34,26 @@ public class RestaurantController {
         try {
             Optional<Restaurant> restaurant = restaurantService.getRestaurantById(id);
             return restaurant.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Restaurant> saveRestaurant(@RequestBody Restaurant restaurant) {
+        try {
+            Restaurant newRestaurant = restaurantService.saveRestaurant(restaurant);
+            return new ResponseEntity<>(newRestaurant, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Restaurant> deleteRestaurant(@PathVariable Integer id) {
+        try {
+            restaurantService.deleteRestaurant(id);
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
---
+++ src/main/java/com/allergenie/api/Services/Utils.java
@@ -0,0 +1,18 @@
+package com.allergenie.api.Services;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public class Utils {
+
+    public static boolean isValidEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
+    }
+}
